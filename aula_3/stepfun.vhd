16d2781f64f26b9175b1656dd26f9015e6ebb565
hfcaio <hfcaio@gmail.com>
2024-03-17 21:22:15 -0300

    stepfun com somador no mesmo arquivo, e analisado

--- aula_3/stepfun.vhd
+++ aula_3/stepfun.vhd
@@ -4,8 +4,31 @@
 -- Description:
 -- VHDL module to convert from hex (4b) to 7-segment
 -------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY somador_32 IS
+    PORT (
+        a, b : IN bit_vector(31 DOWNTO 0);
+        enable : IN BIT;
+        c : OUT bit_vector(31 DOWNTO 0);
+        overflow : OUT BIT
+    );
+END somador_32;
+
+ARCHITECTURE Behave OF somador_32 IS
+    SIGNAL sum : unsigned(32 DOWNTO 0);
+BEGIN
+    sum <= unsigned(TO_STDLOGICVECTOR(a)) + unsigned(TO_STDLOGICVECTOR(b)) WHEN enable = '1' ELSE
+        (OTHERS => '0');
+    c <= TO_BITVECTOR(STD_LOGIC_VECTOR(sum(31 DOWNTO 0)));
+    overflow <= TO_BIT(sum(32));
+END Behave;
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY stepfun IS
     PORT (
@@ -21,7 +44,7 @@ ARCHITECTURE Behave OF stepfun IS
             a, b : IN bit_vector(31 DOWNTO 0);
             enable : IN BIT;
             c : OUT bit_vector(31 DOWNTO 0);
-            overflow : OUT BIT;
+            overflow : OUT BIT
         );
     END COMPONENT;
 
@@ -53,20 +76,20 @@ ARCHITECTURE Behave OF stepfun IS
         );
     END COMPONENT;
 
-    SIGNAL sum1, sum2, sum3, sum4, sum5, sum6 : unsigned(31 DOWNTO 0);
+    SIGNAL sum1_output, sum2, sum3, sum4, sum5, sum6 : bit_vector(31 DOWNTO 0);
     SIGNAL overflow1, overflow2, overflow3, overflow4, overflow5, overflow6 : BIT;
     SIGNAL ch_out, sum1_out, maj_out, sum0_out : bit_vector(31 DOWNTO 0);
 
 BEGIN
-    sum1_port : somador PORT MAP(hi, '1', kpw, overflow1, sum1);
-    sum2_port : somador PORT MAP(ch_out, '1', sum1, overflow2, sum2);
-    sum3_port : somador PORT MAP(sum1_out, '1', sum2, overflow3, sum3);
-    sum4_port : somador PORT MAP(di, '1', sum3, overflow4, sum4);
-    sum5_port : somador PORT MAP(maj_out, '1', sum3, overflow5, sum5);
-    sum6_port : somador PORT MAP(sum0_out, '1', sum5, overflow6, sum6);
+    sum1_port : somador_32 PORT MAP(hi, kpw, '1', sum1_output, overflow1);
+    sum2_port : somador_32 PORT MAP(ch_out, sum1_output, '1', sum2, overflow2);
+    sum3_port : somador_32 PORT MAP(sum1_out, sum2, '1', sum3, overflow3);
+    sum4_port : somador_32 PORT MAP(di, sum3, '1', sum4, overflow4);
+    sum5_port : somador_32 PORT MAP(maj_out, sum3, '1', sum5, overflow5);
+    sum6_port : somador_32 PORT MAP(sum0_out, sum5, '1', sum6, overflow6);
 
     ch_port : ch PORT MAP(ei, fi, gi, ch_out);
-    sum1_port : sum1 PORT MAP(ei, sum1_out);
+    sum1_func_port : sum1 PORT MAP(ei, sum1_out);
     maj_port : maj PORT MAP(ai, bi, ci, maj_out);
     sum0_port : sum0 PORT MAP(ai, sum0_out);
 
@@ -75,7 +98,7 @@ BEGIN
     bo <= ai;
     co <= bi;
     do <= ci;
-    eo <= sum4_out;
+    eo <= sum4;
     fo <= ei;
     go <= fi;
     ho <= gi;
